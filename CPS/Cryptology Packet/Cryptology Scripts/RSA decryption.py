9167c7214162c87bd6ef6206332afd02702a8b6b
SapphireHue <64503620+SapphireHue@users.noreply.github.com>
2024-05-15 09:28:59 -0500

    Pollard Rho implementation

--- CPS/Cryptology Packet/Cryptology Scripts/RSA decryption.py
+++ CPS/Cryptology Packet/Cryptology Scripts/RSA decryption.py
@@ -11,10 +11,41 @@ def trialDivisionFactoring(pq):
         for composite in range(i+i+2, n-1, i+2): # increment by the prime
             isPrime[composite] = 0
 
+def polynomial(x, n, pq):
+    return (x*x + n)%pq
+def PollardRho(pq, n):
+    print("Trying Pollard Rho with n =", n)
+    x = 2
+    y = x
+    d = 1
+    while d == 1: 
+        x = polynomial(x, n, pq)
+        y = polynomial(polynomial(y, n, pq), n, pq)
+        d = math.gcd(abs(x-y), pq)
+        # print(d)
+    if d == n:
+        return False
+    return d
+
 pq = int(input("pq: "))
 E = int(input("E: "))
 Me = int(input("M^e: "))
+
+print("Using trial division:")
 p, q = trialDivisionFactoring(pq)
+privMod = (p-1)*(q-1)
+D = extendedEuclideanAlg.extendedAlgorithm(E, privMod)['a'] if privMod < E else extendedEuclideanAlg.extendedAlgorithm(E, privMod)['b']
+print("Decrypted message:", pow(Me, D, pq))
+
+print("Using Pollard Rho:")
+i = 1
+temp = PollardRho(pq, i)
+while temp == False:
+    i+=1
+    temp = PollardRho(pq)
+p = temp
+q = pq//p
+print(p, q)
 privMod = (p-1)*(q-1)
 D = extendedEuclideanAlg.extendedAlgorithm(E, privMod)['a'] if privMod < E else extendedEuclideanAlg.extendedAlgorithm(E, privMod)['b']
 print("Decrypted message:", pow(Me, D, pq))
